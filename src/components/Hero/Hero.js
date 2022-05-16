cabb01ce8455ee2975626fafa81f17386d071569
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2022-05-16 22:44:21 +0300

    Updated hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello I'm Melhem Rahmeh, <br />
+        And welcome there!
+      </SectionTitle>
+      <SectionText >
+        Being a tech enthusiast since a young age, I started learning to program in High school then chose to major in Computer Science in college. <br />
+        I am highly interested in Software Engineering, as I see it as my future path, and currently strengthening my Web Development and software engineering practices, and looking into Machine Learning.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://www.linkedin.com/in/melhem-rahmeh/' }> Learn More </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
