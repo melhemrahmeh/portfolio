c2962ab35166dde43adb49713458cf674f155db5
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2022-05-16 22:48:03 +0300

    Finalized Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,8 +12,8 @@ const Hero = () => (
         And welcome there!
       </SectionTitle>
       <SectionText >
-        Being a tech enthusiast since a young age, I started learning to program in High school then chose to major in Computer Science in college. <br />
-        I am highly interested in Software Engineering, as I see it as my future path, and currently strengthening my Web Development and software engineering practices, and looking into Machine Learning.
+        Being a tech enthusiast since a young age, I started learning to program in High school then chose to major in Computer Science in college. <br /> <br />
+        I am highly interested in Software Engineering, as I see it as my future path, and currently strengthening my Web Development and Software Engineering practices, and looking into Machine Learning.
       </SectionText>
       <Button onClick={() => window.location = 'https://www.linkedin.com/in/melhem-rahmeh/' }> Learn More </Button>
     </LeftSection>
