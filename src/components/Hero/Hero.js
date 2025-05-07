c00b2ac0c73197039870d7ec3f05c5e5ae1af985
Melhem Rahmeh <melhem.rahmehh@gmail.com>
2025-05-07 21:35:42 +0300

    changed roles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -22,7 +22,7 @@ const Hero = () => (
         the 60 high school graduates in Lebanon to receive the prestigious USAID
         scholarship for undergraduate studies at AUB.
         <br /> I did 2 web development internships and landed 2 professional
-        jobs as a Software Engineer and then as a Business Analyst during my
+        jobs as a Software Engineer and then as a System Analyst during my
         university years, culminating in a successful transition to Murex as a
         DevOps Engineer post-graduation.
       </SectionText>
